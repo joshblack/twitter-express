148f9996f34de90a7fa4567081296c84a83f8efe
Josh Black <joshblack@us.ibm.com>
2016-06-27 20:17:11 -0500

    Add routes for Tweets and a single Tweet

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const tweets = [];
 
 for (let i = 0; i < 20; i++) {
   tweets[i] = {
+    id: `${i + 1}`,
     body: faker.lorem.sentences(),
     author: {
       username: faker.internet.userName(),
@@ -19,16 +20,37 @@ for (let i = 0; i < 20; i++) {
 // RESTful API
 
 // GET /tweets -> Get all the tweets
+server.get('/tweets', (req, res) => {
+  res.json(tweets);
+});
+
 // GET /tweets/1 -> Get the tweet with the id of 1
+server.get('/tweets/:id', (req, res) => {
+  const { id } = req.params;
+
+  // findTweetById: [tweet] | []
+  const findTweetById = tweets.filter((tweet) => {
+    if (tweet.id === id) {
+      return true;
+    }
+
+    return false;
+  });
+
+  if (findTweetById.length > 0) {
+    res.json(findTweetById[0]);
+  } else {
+    res.status(404).json({
+      message: `Tweet not found with id: ${id}`
+    });
+  }
+});
+
 // GET /tweets/new -> Display a form for making a new Tweet
 // POST /tweets with { tweet } -> Make a new Tweet with the body
 // GET /tweets/edit -> Display a form for editing a Tweet
 // PUT /tweets/1 with { tweet }-> Edit a Tweet with the body
 // DELETE /tweets/1 -> Delete a tweet
-
-server.get('/', (req, res) => {
-  res.json(tweets);
-});
 
 server.listen(3000, () => {
   console.log('Listening on http://localhost:3000');
