7fa926d9c86454512435eea0c9873d1969390685
Josh Black <joshblack@us.ibm.com>
2016-06-27 20:52:01 -0500

    Add a test harness for application

---
+++ test/tweets-test.js
@@ -0,0 +1,9 @@
+const expect = require('expect');
+
+describe('Tweets', () => {
+  describe('#index', () => {
+    it('should return a list of tweets', () => {
+      expect(true).toBe(false);
+    });
+  });
+});
